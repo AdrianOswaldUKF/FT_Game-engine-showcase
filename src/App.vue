860e1c7afc1a4e72144b8c93511fcb6d6be7ae92
AdrianOswaldUKF <adrian.oswald@student.ukf.sk>
2024-12-30 14:21:46 +0100

    Pridaný Vuetify a oprava .vue v TypeScript

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,15 @@
 </template>
 
 
-<script>
+<script lang="ts">
 import HomeView from "@/views/HomeView.vue";
 
 export default {
 
   components: {
+
     HomeView
+
   },
 
 }
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,28 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// App.vue and Vue Router
 import App from './App.vue'
 import router from './router'
 
+// Material Design Icons
+import '@mdi/font/css/materialdesignicons.css';
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 const app = createApp(App)
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
